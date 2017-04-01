69f889383e45b4e67e221560ac3348e294211fdc
KURURU <i@okako.com>
2017-04-01 18:07:03 +0900

    1

--- lcd_i2c.c
+++ lcd_i2c.c
@@ -1,3 +1,8 @@
+// cc -o lcd_i2c lcd_i2c.c -lwiringPi
+#include <stdio.h>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
 // Define some device parameters
 #define I2C_ADDR   0x27 // I2C device address
 #define LCD_WIDTH  16   // Maximum characters per line
@@ -17,24 +22,32 @@
 #define ENABLE  0b00000100 // Enable bit
 
 // Timing constants
-#define E_PULSE  0.0005
-#define E_DELAY  0.0005
+#define E_PULSE  0.5
+#define E_DELAY  0.5
+
+int lcd_toggle_enable(int bits) {
+  // Toggle enable
+  Delay(E_DELAY);
+  wiringPiI2CWrite(I2C_ADDR, (bits | ENABLE))
+  Delay(E_PULSE)
+  wiringPiI2CWrite(I2C_ADDR,(bits & ~ENABLE));
+  Delay(E_DELAY);
+};
 
 int lcd_byte(int bits, int mode) {
   // Send byte to data pins
   // bits = the data
   // mode = 1 for data
   //        0 for command
+  int bits_high = mode | (bits & 0xF0)      | LCD_BACKLIGHT;
+  int bits_low  = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT;
 
-  bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
-  bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
-
-  # High bits
-  bus.write_byte(I2C_ADDR, bits_high)
+  // High bits
+  wiringPiI2CWrite(I2C_ADDR, bits_high);
   lcd_toggle_enable(bits_high)
 
-  # Low bits
-  bus.write_byte(I2C_ADDR, bits_low)
-  lcd_toggle_enable(bits_low)
+  // Low bits
+  wiringPiI2CWrite(I2C_ADDR, bits_low);
+  lcd_toggle_enable(bits_low);
 };
 
