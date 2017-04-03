52bbe6ebf458fd76e3216ac158d667f1c394d541
imaicom <i@okako.com>
2017-04-03 12:42:17 +0900

    modified:   lcd.py

--- lcd.py
+++ lcd.py
@@ -4,5 +4,9 @@ from time import *
 lcd = lcddriver.lcd()
 
 #lcd.lcd_display_string("11111111111111111111222222222222222222223333333333333333333344444444444444444444", 1)
-lcd.lcd_display_string("1234567890123456789012345678901234567890abcdefghijklmnop", 1)
-lcd.lcd_display_string("923456789012345Z789012345678901234567890aBcdefghijklmnop", 1)
+#lcd.lcd_display_string("1234567890123456789012345678901234567890abcdefghijklmnop", 1)
+#lcd.lcd_display_string("923456789012345Z789012345678901234567890aBcdefghijklmnop", 1)
+
+
+lcd.lcd_display_string("abcdefghjuklmn",1)
+lcd.lcd_display_string("9999",2)
